64625585e72aa788ff78cd55d099960a7b85cf8a
Anddy <alonzoa2015@gmail.com>
2022-06-08 16:22:20 -0700

    get all fish working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,24 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { fishs } = require('../data/fishs');
 
 describe('fs-any-api routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+
+  it('GET /api/fishs responds an array of fishs with name and id', async () => {
+    const res = await request(app).get('/api/fishs');
+    const expected = fishs.map(fish => {
+      return {
+        id: fish.id,
+        name: fish.name
+      };
+    });
+    expect(res.body).toEqual(expected);
+
   });
 
   afterAll(() => {
